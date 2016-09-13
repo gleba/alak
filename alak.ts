3ff4d2c3daad4f428b571dc4bec2078210ea1c2f
Gleb YP <dev@gleb.pw>
2016-09-13 21:14:48 +0300

    v0.4.12

---
+++ alak.ts
@@ -0,0 +1,86 @@
+import {IAStream} from "./ialak";
+
+function start(value?, mixer?: Function):IAStream {
+    let listeners = [];
+    const streamFn = (v)=> {
+        const updateValue = (v)=> {
+            if (mixer) v = mixer(v)
+            streamFn.value = v;
+            listeners.forEach(f=>f(v));
+        }
+        if (v) {
+            if (v.then) v.then(updateValue)
+            else updateValue(v)
+        }
+        return streamFn.value;
+    };
+
+    Object.assign(streamFn, {
+        on: fn=> {
+            listeners.push(fn)
+            if (streamFn.value) {
+                fn(streamFn.value)
+            }
+        },
+        silent: fn=>streamFn.value = fn(streamFn.value),
+        promise: ()=>new Promise((done)=>streamFn.on(done)),
+        end: ()=> {
+            streamFn.value = null
+            listeners = null
+            streamFn = null
+        }
+    })
+
+    if (value) streamFn(value)
+    return streamFn;
+}
+
+const A = {
+    once(come: Function){
+        let wave = start()
+        come((v)=>{
+            wave(v)
+            wave.end()
+        })
+        return wave
+    },
+    start:start,
+    mix(...ar){
+        let newStream = A.start()
+        let active = new Map()
+        const emit = () => {
+            if (active.size == ar.length)
+                newStream(Array.from(active.values()))
+        }
+
+        ar.forEach(
+            stream=> {
+                stream.on(
+                    data=> {
+                        active.set(stream, data)
+                        emit()
+                    }
+                )
+            }
+        )
+        return newStream
+    },
+    match(value, pattern, data): void {
+        let resp = value
+        if (data) resp = data
+        for (let key of Object.keys(pattern)) {
+            if (value == key) {
+                pattern[key](resp)
+                return
+            }
+        }
+        if (pattern["*"]) {
+            pattern["*"](resp)
+        }
+    },
+    "default": ""
+}
+
+
+// A.default = A
+export = A
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const A = require('./index')
+const A = require('./alak')
 
 
 let stream1 = A.start()
@@ -8,10 +8,11 @@ stream1.on(console.log)
 stream1("stream1 subscribe ok")
 
 
-
-A.mix(stream1,stream2).on(r=>{
-    console.log("mix",r.length)
-})
+A.mix(stream1, stream2).on(
+    r=> {
+        console.log("mix", r.length)
+    }
+)
 
 stream2("stream2 1")
 //stream2("stream1 2")
@@ -21,3 +22,27 @@ stream1.on(
         console.log("new subsribe", data);
     }
 )
+
+//let sa = A.new("sss")
+//sa.on(console.log)
+//
+//sa.emit("xxx!")
+
+
+let fut = A.once(
+    done=> {
+        setTimeout(
+            ()=> {
+                done(1000)
+            }, 1000
+        )
+    }
+);
+
+fut.on(x=>{
+    console.log("done",x)
+    console.log("fut",fut());
+    setTimeout(()=>{
+        console.log("fut",fut);
+    },1000)
+})
