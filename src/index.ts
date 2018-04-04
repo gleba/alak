fdf15a33cdcf6faec31b38fa9441c8d0627d5229
Gleb YP <dev@gleb.pw>
2018-04-04 05:22:12 +0300

    v0.8.24

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,37 @@ import {patternMatch} from "./match";
 
 
 export interface DChannel<T extends any> {
-    (...a: T[]): T
+  (...a: T[]): T
 
-    v: T
-    data: T[]
+  v: T
+  data: T[]
 
-    on(fn: Listener<T>): DChannel<T>
+  on(fn: Listener<T>): DChannel<T>
 
-    end(): void
-    emit(): void
+  off(fn: Listener<T>): DChannel<T>
 
-    match(...pattern)
+  stateless(v?: boolean): DChannel<T>
 
-    mutate(fn: Listener<T>): T
+  emitter(v?: boolean): DChannel<T>
 
-    branch<U>(fn: (...a: any[]) => U[]): DChannel<U>
+  end(): void
 
-    stop(fn): void
+  emit(): void
 
-    drop(): void
+  match(...pattern)
 
-    inject(obj: any, key?: string): void
+  mutate(fn: Listener<T>): T
 
-    reject(obj): void
+  branch<U>(fn: (...a: any[]) => U[]): DChannel<U>
+
+  stop(fn): void
+
+  drop(): void
+
+  inject(obj: any, key?: string): void
+
+  reject(obj): void
+
 
 }
 
@@ -34,152 +42,175 @@ export interface DChannel<T extends any> {
  * Create new channel
  */
 export default function DFlow<T>(...a: T[]): DChannel<T> {
-    type Fn = Listener<T>
-    let listeners = []
-    let mapObjects: any //Map<any, Function>
-    let proxy = {
-        data: [],
-        on: function (fn: Fn) {
-            listeners.push([this, fn])
-            if (proxy.data.length > 0)
-                fn.apply(this, proxy.data)
-        },
-        end: () => {
-            deleteParams(functor)
-            deleteParams(proxy)
-            listeners = null
-            proxy = null
-        },
-        emit: () => {
-            listeners.forEach(f => f[1].apply(f[0], proxy.data))
-        },
-        mutate: function (fn: Fn) {
-            let newValue
+  type Fn = Listener<T>
+  let listeners = []
+  let state = true
+  let emitter = false
+  let mapObjects: any //Map<any, Function>
+  let proxy = {
+    data: [],
+    on: function (fn: Fn) {
+      listeners.push([this, fn])
+      if (proxy.data.length > 0)
+        fn.apply(this, proxy.data)
+    },
+    end: () => {
+      deleteParams(functor)
+      deleteParams(proxy)
+      listeners = null
+      proxy = null
+    },
+    emit: () => {
+      listeners.forEach(f => f[1].apply(f[0], proxy.data))
+    },
+    mutate: function (fn: Fn) {
+      let newValue
 
-            if (proxy.data.length > 1) {
-                newValue = fn.apply(this, proxy.data)
-                setValues(newValue)
-            } else {
-                newValue = fn.apply(this, [getValue()])
-                setValues([newValue])
-            }
-        },
-        match: function () {
-            proxy.on(A.match(arguments))
-        },
-        drop: () => listeners = [],
-        stop(fn) {
-            remove(listeners, fn)
-        },
-        branch(f) {
-            let newCn = DFlow()
-            proxy.on((...v) => newCn(...f(...v)))
-            return newCn
-        },
-        inject(obj?: any, key?: string) {
-            if (!obj) obj = {}
-            if (!mapObjects) mapObjects = new Map<any, Function>()
-            let fn = key
-                ? v => obj[key] = v
-                : v => Object.keys(v).forEach(k => obj[k] = v[k])
-            mapObjects.set(obj, fn)
-            proxy.on(fn)
-            return obj
-        },
-        reject(obj) {
-            if (mapObjects.has(obj)) {
-                proxy.stop(mapObjects.get(obj))
-                mapObjects.delete(obj)
-            }
-        }
+      if (proxy.data.length > 1) {
+        newValue = fn.apply(this, proxy.data)
+        setValues(newValue)
+      } else {
+        newValue = fn.apply(this, [getValue()])
+        setValues([newValue])
+      }
+    },
+    match: function () {
+      proxy.on(A.match(arguments))
+    },
+    drop: () => listeners = [],
+    stop(fn) {
+      remove(listeners, fn)
+    },
+    branch(f) {
+      let newCn = DFlow()
+      proxy.on((...v) => newCn(...f(...v)))
+      return newCn
+    },
+    inject(obj?: any, key?: string) {
+      if (!obj) obj = {}
+      if (!mapObjects) mapObjects = new Map<any, Function>()
+      let fn = key
+        ? v => obj[key] = v
+        : v => Object.keys(v).forEach(k => obj[k] = v[k])
+      mapObjects.set(obj, fn)
+      proxy.on(fn)
+      return obj
+    },
+    reject(obj) {
+      if (mapObjects.has(obj)) {
+        proxy.stop(mapObjects.get(obj))
+        mapObjects.delete(obj)
+      }
+    },
+    stateless(on = true) {
+
+      state = !on
+      return this
+    },
+    emitter(on = true) {
+      emitter = on
+      return this
     }
+  }
 
-    const getValue = () => proxy.data ? proxy.data.length > 1 ? proxy.data : proxy.data[0] : null
-    const setValues = v => {
-        if (v.length > 0) {
-            functor['data'] = proxy.data = v
-            functor['v'] = v ? v.length > 1 ? v : v[0] : null
-            listeners.forEach(f => f[1].apply(f[0], v))
-        }
+  const getValue = () => {
+    return proxy.data ? proxy.data.length > 1 ? proxy.data : proxy.data[0] : null
+  }
+  const setValues = v => {
+    if (v.length > 0 || emitter) {
+      if (state) {
+        functor['data'] = proxy.data = v
+        functor['v'] = v ? v.length > 1 ? v : v[0] : null
+      }
+      listeners.forEach(f => f[1].apply(f[0], v))
     }
+  }
 
-    function functor(...a) {
-        if (!proxy) {
-            console.error("emit ended channel: " + a)
-            return
-        }
-        setValues(Object.values(arguments))
-        return getValue()
+  function functor(...a) {
+    if (!proxy) {
+      console.error("emit ended channel: " + a)
+      return
     }
+    setValues(Object.values(arguments))
+    return getValue()
+  }
 
-    let v = Object.values(arguments)
-    setValues(v)
-    Object.assign(functor, proxy)
-    return functor as any as DChannel<T>
+  let v = Object.values(arguments)
+  setValues(v)
+
+
+  Object.assign(proxy, {
+    off: proxy.stop,
+  })
+  Object.assign(functor, proxy)
+  return functor as any as DChannel<T>
 }
 
 
 export class DInjectableFlow {
-    mapObjects: Map<any, Function>
+  mapObjects: Map<any, Function>
 
-    inject(obj?) {
-        if (!obj) obj = {}
-        if (!this.mapObjects) this.mapObjects = new Map<any, Function>()
-        Object.keys(this).forEach(k => {
-            let f = this[k]
-            if (f.on) {
-                obj[k] = f.data[0]
-                let fn = v => obj[k] = v
-                f.on(fn)
-                this.mapObjects.set(obj, fn)
-            }
-        })
-        return obj
-    }
+  inject(obj?) {
+    if (!obj) obj = {}
+    if (!this.mapObjects) this.mapObjects = new Map<any, Function>()
+    Object.keys(this).forEach(k => {
+      let f = this[k]
+      if (f.on) {
+        obj[k] = f.data[0]
+        let fn = v => obj[k] = v
+        f.on(fn)
+        this.mapObjects.set(obj, fn)
+      }
+    })
+    return obj
+  }
 
-    to(obj) {
-        Object.keys(this).forEach(k=>{
-            let flow = this[k]
-            if (flow.on){
-                flow.on(obj[k])
-            }
-        })
-    }
-    
-    from(obj) {
-        Object.keys(this).forEach(k=>{
-            let flow = obj[k]
-            if (flow.on){
-                flow.on(this[k])
-            }
-        })
-    }
+  to(obj) {
+    Object.keys(this).forEach(k => {
+      let flow = this[k]
+      if (flow.on) {
+        flow.on(obj[k])
+      }
+    })
+  }
+
+  from(obj) {
+    Object.keys(this).forEach(k => {
+      let flow = obj[k]
+      if (flow.on) {
+        flow.on(this[k])
+      }
+    })
+  }
 }
 
 
 export const A = {
-    start: DFlow,
-    mix(...ar) {
-        let newStream = DFlow()
-        let active = new Map()
-        const emit = () => {
-            if (active.size == ar.length)
-                newStream(Array.from(active.values()))
-        }
+  start: DFlow,
+  flow: DFlow,
+  stateless: () => DFlow().stateless(true),
+  emitter: (...ar) => DFlow(...ar).emitter(true),
+  mix(...ar) {
+    let newStream = DFlow()
+    let active = new Map()
+    const emit = () => {
+      if (active.size == ar.length)
+        newStream(Array.from(active.values()))
+    }
 
-        ar.forEach(
-            stream => {
-                stream.on(
-                    data => {
-                        active.set(stream, data)
-                        emit()
-                    }
-                )
-            }
+    ar.forEach(
+      stream => {
+        stream.on(
+          data => {
+            active.set(stream, data)
+            emit()
+          }
         )
-        return newStream
-    },
-    match: patternMatch
+      }
+    )
+    return newStream
+  },
+  match: patternMatch
 }
 
 export type Listener<T extends any> = (...a: T[]) => any
--- tests/base.ts
+++ tests/base.ts
@@ -5,83 +5,103 @@ import DFlow from "../src";
 
 test("Alak test", (t: any) => {
 
-    const a1 = DFlow()
+  const a1 = DFlow()
 
-    a1.on(x => t.fail("end test"))
-    a1.end()
-    a1(0)
+  a1.on(x => t.fail("end test"))
+  a1.end()
+  a1(0)
 
-    const s1 = DFlow([0, 0, 0], 5 as any)
-    s1.on((v1: any, v2) => {
-        t.ok(v1.length == 3 && v2 == 5, "base");
-    })
-
-
-    const s2 = DFlow()
-    s2.on(x => {
-        t.ok(x == 4, "base+ " + x);
-    })
-    s2(4)
-    s2(4, 0)
+  const s1 = DFlow([0, 0, 0], 5 as any)
+  s1.on((v1: any, v2) => {
+    t.ok(v1.length == 3 && v2 == 5, "base");
+  })
 
 
-    const s3 = DFlow()
-    s3.on(x=>{
-        t.ok(true, "emit ok")
-    })
-
-    s3.emit()
-    const s4 = DFlow()
-
-    s4.match(
-        3, x => t.ok(3 == x, "pattern matching by key"),
-        "never", x => t.fail("silent pattern matching *", x)
-    )
-    s4.match(v => [
-        isNumber(v), v => t.ok(typeof v == "number", "fn pattern matching isNumber"),
-        isArray(v), v => t.fail("fn pattern matching"),
-        v == 3, v => t.ok(v == 3, "fn pattern matching +"),
-        v => t.pass('fn pattern matching else')
-    ])
-    s4(3)
-    s4(1)
-    s4("8")
+  const s2 = DFlow()
+  s2.on(x => {
+    t.ok(x == 4, "base+ " + x);
+  })
+  s2(4)
+  s2(4, 0)
 
 
-    let state1 = [5, true, "1"]
-    let state2 = [1, 0, [0, 1]]
-    const s5 = DFlow(Math.random() as any)
-    s5.match(
-        ...state1, v => t.pass('s5: pattern matching user state 1'),
-        ...state2, v => t.pass('s5: pattern matching user state 2'),
-        5, false, "2", (...v) => t.pass('s5: pattern matching user state +'),
-        5, true, "3", (...v) => t.pass('s5: map base done'),
-        (...v) => t.pass('s5: pattern matching else state ' + v),
-    )
+  const s3 = DFlow()
+  s3.on(x => {
+    t.ok(true, "emit ok")
+  })
 
-    s5.match((a, b, c) => [
-        isArray(c), () => t.pass("s5: fn pattern matching isArray(c)"),
-        a == "_" && isString(c), (...v) => t.ok(s5()[0] == "_", "s5: fn pattern matching multi")
-    ])
-    // s5(5, false, "2")
-    // s5(105, true, "2")
-    s5(...state2)
-    s5(...state1)
-    s5.mutate((...v) => {
-        // console.log(v, state1)
-        t.ok(state1[2] == v[2], 's5: map base start')
-        v[2] = "3"
-        v[0] = "_"
-        return v
-    })
+  s3.emit()
+  const s4 = DFlow()
 
-    let s6 = DFlow(1)
-    console.log(s6.data, s6.v)
-    s6(2)
-    console.log(s6.data, s6.v)
+  s4.match(
+    3, x => t.ok(3 == x, "pattern matching by key"),
+    "never", x => t.fail("silent pattern matching *", x)
+  )
+  s4.match(v => [
+    isNumber(v), v => t.ok(typeof v == "number", "fn pattern matching isNumber"),
+    isArray(v), v => t.fail("fn pattern matching"),
+    v == 3, v => t.ok(v == 3, "fn pattern matching +"),
+    v => t.pass('fn pattern matching else')
+  ])
+  s4(3)
+  s4(1)
+  s4("8")
 
-    s6.mutate(x => x+1)
-    console.log(s6.data, s6.v)
 
+  let state1 = [5, true, "1"]
+  let state2 = [1, 0, [0, 1]]
+  const s5 = DFlow(Math.random() as any)
+  s5.match(
+    ...state1, v => t.pass('s5: pattern matching user state 1'),
+    ...state2, v => t.pass('s5: pattern matching user state 2'),
+    5, false, "2", (...v) => t.pass('s5: pattern matching user state +'),
+    5, true, "3", (...v) => t.pass('s5: map base done'),
+    (...v) => t.pass('s5: pattern matching else state ' + v),
+  )
+
+  s5.match((a, b, c) => [
+    isArray(c), () => t.pass("s5: fn pattern matching isArray(c)"),
+    a == "_" && isString(c), (...v) => t.ok(s5()[0] == "_", "s5: fn pattern matching multi")
+  ])
+  // s5(5, false, "2")
+  // s5(105, true, "2")
+  s5(...state2)
+  s5(...state1)
+  s5.mutate((...v) => {
+    t.ok(state1[2] == v[2], 's5: mutate in')
+    v[2] = "3"
+    v[0] = "_"
+    return v
+  })
+  t.ok(s5.v[2] == '3', 's5: mutate out')
+
+
+  let s6 = DFlow(1)
+  t.ok(s6.data == s6.v as any, 's6.data == s6.v')
+  s6(2)
+  s6.mutate(x => x + 1)
+  t.ok(3 == s6() && s6.data == s6.v as any, 's6')
+
+
+  let s7 = DFlow().stateless()
+  s7("any")
+  let neverFn = v=>t.ok(false, 'neverFn called')
+  s7.on(neverFn)
+  s7.off(neverFn)
+
+  s7.match(
+    'stateless', v=>t.ok(true, 's7: stateless'),
+  )
+
+
+  s7('stateless')
+  s7.on(neverFn)
+  s7()
+  s7.off(neverFn)
+  s7.on(v=>{
+    t.ok(true, 's7: emitter')
+  })
+  s7.emitter(true)
+  s7()
 
 })
